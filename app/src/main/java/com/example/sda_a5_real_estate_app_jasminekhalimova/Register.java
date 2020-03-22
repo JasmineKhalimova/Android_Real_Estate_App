b69acf8686a5bf36cc8c9dab79f180619f0cc0c2
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-03-22 16:12:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sda_a5_real_estate_app_jasminekhalimova/Register.java
@@ -0,0 +1,14 @@
+package com.example.sda_a5_real_estate_app_jasminekhalimova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

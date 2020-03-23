52b26f826ba40e0636ca6a5021be0c82b013941e
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-03-22 20:31:37 -0400

    Fist initial Commit. User Case 1.2: User Account Page: Pulling user info from firebase cloud storage

--- app/src/main/java/com/example/sda_a5_real_estate_app_jasminekhalimova/MainActivity.java
+++ app/src/main/java/com/example/sda_a5_real_estate_app_jasminekhalimova/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.sda_a5_real_estate_app_jasminekhalimova;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+    //Declaring variables
+    TextView fullName,email;
+
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullName = findViewById(R.id.profileName);
+        email    = findViewById(R.id.profileEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        // Fetching User details from firebase cloud storage
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                fullName.setText(documentSnapshot.getString("fName"));
+                email.setText(documentSnapshot.getString("email"));
+            }
+        });
     }
 
     // Logout button from the user account
